989c48e07a852d582b04ccc5b679f36047fbaea2
Shvetkoy Anton <121558506+megrIm-sa@users.noreply.github.com>
2023-11-05 00:08:15 +0600

    Add files via upload

--- form_validation.js
+++ form_validation.js
@@ -1,16 +1,50 @@
-const form = document.getElementById("myForm");
+const formObject = {
+    formName: "myForm",
+    inputName: "email",
+    pattern: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+
+    checkPattern: function (input) {
+        let valid = true;
+        if (!formObject.pattern.test(input.value)) {
+            valid = false;
+            alert(`Enter a valid ${formObject.inputName}!`);
+        }
+        return valid;
+    },
+};
+
+const form = document.getElementById(formObject.formName);
 
 form.addEventListener("submit", function (event) {
-    let valid = true;
-    const emailInput = document.getElementById("email");
-
-    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if (!emailPattern.test(emailInput.value)) {
-        valid = false;
-        alert("Enter a valid email address");
-    }
-
-    if (!valid) {
+    const input = document.getElementById(formObject.inputName);
+    //console.log("input is"input);
+    if (!formObject.checkPattern(input)) {
         event.preventDefault(); // Prevent form submission if there are validation errors
+        return;
     }
+    var formSelect = document.querySelector('form');
+    formSelect.style.display = 'none';
+    formSelect.nextElementSibling.innerHTML = "Thank you! We will contact you!";
+    notifyMe();
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (!Notification) {
+        alert('Desktop notifications not available in your browser. Try Chromium.');
+        return;
+    }
+
+    if (Notification.permission !== "granted")
+        Notification.requestPermission();
 });
+
+function notifyMe() {
+    if (Notification.permission !== "granted")
+        Notification.requestPermission();
+    else {
+        console.log("notification showing");
+        var notification = new Notification('Notification title', {
+            body: "Hey there! You've been notified!",
+        });
+    }
+}
--- script.js
+++ script.js
@@ -13,3 +13,26 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+//Hover button effect
+const button = document.querySelector('button');
+
+button.addEventListener('mouseover', () => {
+  button.style.backgroundColor = 'lightgrey';
+});
+
+button.addEventListener('mouseout', () => {
+  button.style.backgroundColor = '';
+});
+
+//Hover card effect
+const card = document.querySelector('img');
+
+card.addEventListener('mouseover', () => {
+  //card.style.backgroundColor = 'lightgrey';
+  console.log("mouseover");
+});
+
+card.addEventListener('mouseout', () => {
+  //card.style.backgroundColor = '';
+});
